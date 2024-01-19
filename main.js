baf1a43c8e5b2cdf14c8b3ca1b5a550f192fb67c
agnesvarga1 <agikolyok@gmail.com>
2024-01-19 15:11:39 +0100

    created obj arr + log ogni obj

--- main.js
+++ main.js
@@ -1,1 +1,59 @@
+/**
+ MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite.
+MILESTONE 1:
+Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe
+BONUS 1:
+Trasformare la stringa foto in una immagine effettiva
+BONUS 2:
+Organizzare i singoli membri in card/schede
+ */
 
+////////////////////////////DATA//////////////////////////////
+// Wayne Barnett	Founder & CEO	wayne-barnett-founder-ceo.jpg
+// Angela Caroll	Chief Editor	angela-caroll-chief-editor.jpg
+// Walter Gordon	Office Manager	walter-gordon-office-manager.jpg
+// Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
+// Scott Estrada	Developer	scott-estrada-developer.jpg
+// Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg
+
+const team = [
+  {
+    fullName: "Wayne Barnett",
+    position: "Founder & CEO",
+    photo: "wayne-barnett-founder-ceo.jpg",
+  },
+  {
+    fullName: "Angela Caroll",
+    position: "Chief Editor",
+    photo: "angela-caroll-chief-editor.jpg",
+  },
+  {
+    fullName: "Walter Gordon",
+    position: "Office Manager",
+    photo: "walter-gordon-office-manager.jpg",
+  },
+  {
+    fullName: "Angela Lopez",
+    position: "Social Media Manager",
+    photo: "angela-lopez-social-media-manager.jpg",
+  },
+  {
+    fullName: "Scott Estrada",
+    position: "Developer",
+    photo: "scott-estrada-developer.jpg",
+  },
+  {
+    fullName: "Barbara Ramos",
+    position: "Graphic Designer",
+    photo: "barbara-ramos-graphic-designer.jpg",
+  },
+];
+
+for (let i = 0; i < team.length; i++) {
+  for (let key in team[i]) {
+    console.log(team[i][key]);
+  }
+}
